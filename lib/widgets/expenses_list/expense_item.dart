7fba40357abb8a52cfe3816fd4fb189d29567147
auronvila <auronvila1121@gmail.com>
2024-09-22 11:00:03 +0300

    fix indentation

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -10,41 +10,42 @@ class ExpenseItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                style: Theme.of(context).textTheme.titleLarge,
-                expense.title,
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              Row(
-                children: [
-                  Text(
-                    '\$ ${expense.amount.toStringAsFixed(2)}',
-                  ),
-                  const Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Icon(
-                        categoryIcons[expense.category],
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      Text(
-                        expense.getFormatedDate,
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ],
-          )),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              style: Theme.of(context).textTheme.titleLarge,
+              expense.title,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Text(
+                  '\$ ${expense.amount.toStringAsFixed(2)}',
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(
+                      categoryIcons[expense.category],
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      expense.getFormatedDate,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
